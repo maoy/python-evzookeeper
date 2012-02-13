ac62add2a3923bfb1441aa463b6cc92b24410654
Yun Mao <yunmao@gmail.com>
2012-02-13 14:08:28 -0500

    demo on how to use membership

---
+++ evzookeeper/demo_membership.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2011-2012 Yun Mao <yunmao at gmail dot com>.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import eventlet
+import recipes
+
+from evzookeeper import ZKSession, utils
+
+class NodeManager(object):
+    def __init__(self, name):
+        self._session = ZKSession("localhost:2181", 10)
+        self.membership = recipes.Membership(self._session, "/basedir")
+        self.pc = utils.PipeCondition()
+        self.membership.join(name)
+        print 'in __init__', self.membership.get_all(self.pc)
+        eventlet.spawn(self.monitor)
+
+    def monitor(self):
+        while 1:
+            self.pc.wait()
+            print "changed"
+            self.pc = utils.PipeCondition()
+            print 'in monitor', self.membership.get_all(self.pc)
+        
+def demo():
+    nm1 = NodeManager("node1")
+    nm2 = NodeManager("node2")
+    #print nm1.membership.get_all()
+    eventlet.sleep(10)
+    
+if __name__=="__main__":
+    demo()
